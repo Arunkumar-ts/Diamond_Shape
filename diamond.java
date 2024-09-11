12eee9431029ebf3ac8371a9c93470dd576165c7
Arunkumar-ts <tsarunkumar2005pc@gmail.com>
2024-09-11 19:51:56 +0530

    Implemented the diamond shape successfully

--- diamond.java
+++ diamond.java
@@ -7,5 +7,56 @@ class diamond{
         int a=scan.nextInt();
         System.out.println("Given number is : "+a);
         scan.close();
+        int f=0,l=0;
+        if(a%2!=0){
+            f=a/2+1;
+            l=a/2+1;
+            for(int i=1;i<=a;i++){
+                for(int j=1;j<=a;j++){
+                    if(f<=j && l>=j){
+                        System.out.print("* ");
+                    }
+                   else{
+                       System.out.print("  ");
+                   }
+               }
+               System.out.println();
+               if(a/2+1>i){
+                f-=1;
+                l+=1;
+               }
+               else{
+                f+=1;
+                l-=1;
+               }
+            }
+        }
+        else{
+            f=a/2;
+            l=a/2+1;
+            for(int i=1;i<=a;i++){
+                for(int j=1;j<=a;j++){
+                    if(f<=j && l>=j){
+                        System.out.print("* ");
+                    }
+                   else{
+                       System.out.print("  ");
+                   }
+               }
+               System.out.println();
+               if(a/2>i){
+                f-=1;
+                l+=1;
+               }
+               else if( i==a/2 ){
+                f=1;
+                l=a;
+               }
+               else{
+                f+=1;
+                l-=1;
+               }
+            }
+        }
     }
 }
