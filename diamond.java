bc706e9bb7e254caf6b125aaecd77f0563de7570
Arunkumar-ts <tsarunkumar2005pc@gmail.com>
2024-09-11 19:45:50 +0530

    Created Diamond.java File

---
+++ diamond.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class diamond{
+    public static void main(String[] args) {
+        System.err.println("Welcomm..");
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter a number :");
+        int a=scan.nextInt();
+        System.out.println("Given number is : "+a);
+        scan.close();
+    }
+}
